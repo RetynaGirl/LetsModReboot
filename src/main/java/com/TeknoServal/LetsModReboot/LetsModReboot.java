5061e3b4b9447607b075cdf1445df1c30113ae4c
TeknoServal <author@example.com>
2019-03-31 12:08:43 -0600

    yo yo yo

---
+++ src/main/java/com/TeknoServal/LetsModReboot/LetsModReboot.java
@@ -0,0 +1,8 @@
+package com.TeknoServal.LetsModReboot;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "LetsModReboot", name = "Lets Mod Reboot", version = "1.12.2-1.0")
+public class LetsModReboot {
+
+}
